73a59a54f8dea3caa4a03c007aab4ed8d66ecd4f
s2-sengar <sudhanshu.iem2k18@gmail.com>
2024-09-07 18:07:58 +0530

    add data models and placeholders for schemas

---
+++ src/model/Project.ts
@@ -0,0 +1,36 @@
+import mongoose, {Schema,Document} from "mongoose";
+
+export interface Sphere extends Document{
+    userId:string,
+    createdAt:Date,
+    updatedAt:Date,
+    title:string,
+    description?:string
+}
+
+export const SphereSchema:Schema<Sphere> = new Schema({
+    userId:{
+        type:String,
+        required:true
+    },
+    title:{
+        type:String,
+        required:true
+    },
+    description:String,
+    createdAt:{
+        type:Date,
+        required:true,
+        default:Date.now
+    },
+    updatedAt:{
+        type:Date,
+        required:true,
+        default:Date.now
+    },
+})
+
+
+export const SphereModel=
+    (mongoose.models.Sphere as mongoose.Model<Sphere>) || 
+    mongoose.model('Sphere',SphereSchema)
