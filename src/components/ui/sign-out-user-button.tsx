0d844c0452e1abf798518c09bff19637562612d6
s2-sengar <sudhanshu.iem2k18@gmail.com>
2024-09-11 08:17:00 +0530

    make signout button generic with more cutomization flexibility

--- src/components/ui/sign-out-user-button.tsx
+++ src/components/ui/sign-out-user-button.tsx
@@ -5,13 +5,15 @@ import { ExitIcon } from '@radix-ui/react-icons'
 import { useClerk } from '@clerk/nextjs'
 import { cn } from '@/lib/utils'
 const SignOutUser = ({
-    className
+    className,
+    variant
 }:{
-    className?:string
+    className?:string,
+    variant?:"link" | "default" | "destructive" | "outline" | "secondary" | "ghost" | "rounded" | null | undefined
 }) => {
   const { signOut } = useClerk()
   return (
-    <Button variant="secondary" className={cn('h-10 hover:bg-background/20 w-full gap-4 px-7 justify-start font-normal',className)} onClick={() => signOut({ redirectUrl: '/' })}>
+    <Button variant={variant ?? "secondary"} className={cn('h-10 hover:bg-background/20 w-full gap-4 px-7 justify-start font-normal',className)} onClick={() => signOut({ redirectUrl: '/' })}>
         <ExitIcon/>
         Sign out
     </Button>
