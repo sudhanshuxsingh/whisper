2da980a33ede1becdcb32d8d39ba6f4a6dd282c6
s2-sengar <sudhanshu.iem2k18@gmail.com>
2024-09-14 17:25:45 +0530

    Remove unused placeholder data, update navigation data for authorized user

---
+++ src/app/(protected)/_data/home-navigation-tab-data.tsx
@@ -0,0 +1,33 @@
+export const DASHBOARD_HOME_TAB_LIST = [
+  {
+    content: 'Overview',
+    href: '/overview',
+  },
+  {
+    content: 'Analytics',
+    href: '/analytics',
+  },
+  {
+    content: 'User Guide',
+    href: '/user-guide',
+  },
+];
+
+export const DASHBOARD_HOME_TAB_LIST_SPHERE = [
+  {
+    content: 'Overview',
+    href: '/overview',
+  },
+  {
+    content: 'Sphere',
+    href: '/sphere',
+  },
+  {
+    content: 'Analytics',
+    href: '/analytics',
+  },
+  {
+    content: 'User Guide',
+    href: '/user-guide',
+  },
+];
