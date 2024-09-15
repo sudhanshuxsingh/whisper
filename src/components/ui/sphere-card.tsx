a6093e2203794292961cb88702026a2c98ec05af
s2-sengar <sudhanshu.iem2k18@gmail.com>
2024-09-15 06:05:54 +0530

    update sphere card and add placeholder data

--- src/components/ui/sphere-card.tsx
+++ src/components/ui/sphere-card.tsx
@@ -1,15 +1,29 @@
 import Link from 'next/link';
 import React from 'react';
-import { ArrowRightIcon } from '@radix-ui/react-icons';
-// type
+import { ArrowRightIcon, CrumpledPaperIcon } from '@radix-ui/react-icons';
 
-const DashboardCard = () => {
+const SphereCard = () => {
   return (
     <Link
       href={'/sphere/122'}
       className="group/dashboard-card rounded-xl bg-secondary p-1 pb-0"
     >
       <div className="relative flex h-5/6 flex-1 flex-col justify-between overflow-hidden rounded-lg border border-primary/15 bg-background/70 px-4 py-3">
+        <div className="flex items-center gap-2 text-sm text-muted-foreground">
+          <CrumpledPaperIcon />
+          <p className="truncate text-xs">
+            http://localhost:3000/spher/iu234gd742243werew
+          </p>
+        </div>
+        <div className="">
+          <p className="mb-2 truncate">Feedback for whisper</p>
+          <p className="line-clamp-2 text-xs text-muted-foreground">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
+            eos id, repellendus facilis voluptas natus quam reiciendis ipsa
+            fugiat eligendi nisi, eveniet porro beatae earum placeat. At cumque
+            voluptatibus minus.
+          </p>
+        </div>
         <div className="absolute -top-2 left-1/2 h-3 w-2/3 -translate-x-1/2 rounded-b-full bg-indigo-500"></div>
       </div>
       <div className="flex justify-between overflow-hidden px-3 py-2 text-xs font-medium text-muted-foreground">
@@ -24,4 +38,4 @@ const DashboardCard = () => {
   );
 };
 
-export default DashboardCard;
+export default SphereCard;
