575d29b9b71a79ce44681d0dc88b1fdecbe4e765
s2-sengar <sudhanshu.iem2k18@gmail.com>
2024-09-21 10:00:38 +0530

    refactor: add padding to the bottom

--- src/app/(protected)/overview/page.tsx
+++ src/app/(protected)/overview/page.tsx
@@ -7,7 +7,7 @@ import SphereList from './_features/sphere-list';
 
 const Overview = () => {
   return (
-    <Container className="mt-6 w-11/12">
+    <Container className="mt-6 w-11/12 pb-16">
       <h2 className="mb-4 text-2xl font-medium tracking-tight">Spheres</h2>
       <div className="grid auto-rows-[minmax(14rem,_1fr)] grid-cols-[repeat(1,_minmax(16rem,_1fr))] gap-8 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4">
         <CreateSphere />
