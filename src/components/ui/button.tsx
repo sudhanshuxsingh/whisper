3f678dad98b1112218517143ac70ff6cb70a7e59
s2-sengar <sudhanshu.iem2k18@gmail.com>
2024-09-21 15:29:34 +0530

    chore: add xs variant for button

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -26,6 +26,7 @@ const buttonVariants = cva(
         sm: 'h-8 rounded-md px-3 text-xs',
         lg: 'h-10 rounded-md px-8',
         icon: 'h-9 w-9',
+        xs: 'h-6 rounded p-1 text-xs',
       },
     },
     defaultVariants: {
