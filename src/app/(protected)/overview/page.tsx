d736445be4cd3209d6cd046ede88584182624d8f
s2-sengar <sudhanshu.iem2k18@gmail.com>
2024-09-21 07:36:17 +0530

    refactor: remove render functionality from server side

--- src/app/(protected)/overview/page.tsx
+++ src/app/(protected)/overview/page.tsx
@@ -1,20 +1,17 @@
 import Container from '@/components/ui/container';
 import CreateSphere from '@/app/(protected)/overview/_features/create-sphere';
-import SphereCard from '@/components/ui/sphere-card';
-import React from 'react';
-import { getAllSphereAction } from '@/lib/actions/sphere.actions';
+// import SphereCard from '@/components/ui/sphere-card';
+// import React, { Suspense } from 'react';
+// import { getAllSphereAction } from '@/lib/actions/sphere.actions';
+import SphereList from './_features/sphere-list';
 
-const Overview = async () => {
-  const spheres = await getAllSphereAction();
-  console.log({
-    spheres,
-  });
+const Overview = () => {
   return (
     <Container className="mt-6 w-11/12">
       <h2 className="mb-4 text-2xl font-medium tracking-tight">Spheres</h2>
       <div className="grid auto-rows-[minmax(14rem,_1fr)] grid-cols-[repeat(1,_minmax(16rem,_1fr))] gap-8 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4">
         <CreateSphere />
-        <SphereCard />
+        <SphereList />
       </div>
     </Container>
   );
