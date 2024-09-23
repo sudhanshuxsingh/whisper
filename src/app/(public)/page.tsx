29ff8dacf4f411bfb7929c8023beebaaefd49251
s2-sengar <sudhanshu.iem2k18@gmail.com>
2024-09-23 08:50:08 +0530

    feat(landing page): add hero section for the landing page

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,5 +1,22 @@
 import Container from '@/components/ui/container';
+import { Hero, HeroSubtitle, HeroTitle } from './_features/Hero';
 
 export default function Home() {
-  return <Container className="flex flex-col gap-4">Home</Container>;
+  return (
+    <main className="bg-page-gradient">
+      <Container className="flex flex-col gap-4 pt-16">
+        <Hero className="grid place-items-center py-12">
+          <HeroTitle className="translate-y-[-1rem] animate-fade-in tracking-tighter opacity-0 [--animation-delay:200ms]">
+            Go-to platform for anonymous
+            <br className="hidden md:block" /> feedback collection
+          </HeroTitle>
+          <HeroSubtitle className="translate-y-[-1rem] animate-fade-in opacity-0 [--animation-delay:400ms]">
+            Organize feedback with spheres, get AI-driven suggestions, and{' '}
+            <br className="hidden md:block" />
+            integrate seamlessly into your website or app.
+          </HeroSubtitle>
+        </Hero>
+      </Container>
+    </main>
+  );
 }
