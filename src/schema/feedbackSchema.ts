c60ced668c4917ddc41877f7b599a7c25fdf3ef9
s2-sengar <sudhanshu.iem2k18@gmail.com>
2024-09-23 08:33:12 +0530

    fix: add description and optional fields for feedback schema

--- src/schema/feedbackSchema.ts
+++ src/schema/feedbackSchema.ts
@@ -2,7 +2,12 @@ import { z } from 'zod';
 
 export const feedbackSchema = z.object({
   content: z
-    .string()
+    .string({
+      message: "Description can't be empty",
+    })
     .min(10, 'Message must be of at least 10 character')
     .max(300, 'Message can be of at max 300 characters'),
+  name: z.optional(z.string()),
+  email: z.optional(z.string()),
+  rating: z.optional(z.number()),
 });
