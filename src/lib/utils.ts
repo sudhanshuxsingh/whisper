75712194a2af5a47425ec0f88a9c4d36a6e13d34
s2-sengar <sudhanshu.iem2k18@gmail.com>
2024-09-26 08:32:19 +0530

    feat(query params): add query params handlers

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,6 @@
 import { clsx, type ClassValue } from 'clsx';
 import { twMerge } from 'tailwind-merge';
-
+import qs from 'query-string';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
@@ -35,4 +35,46 @@ export function getAbsoutePath(partialPath: string) {
   const { protocol, host } = window.location;
   const fullPath = `${protocol}//${host}/${partialPath}`;
   return fullPath;
+}
+
+type UrlQueryParams = {
+  params: string;
+  key: string;
+  value: string | null;
+};
+
+export function generateUrlWithQuery({ params, key, value }: UrlQueryParams) {
+  const currentUrl = qs.parse(params);
+  currentUrl[key] = value;
+  return qs.stringifyUrl(
+    {
+      url: window.location.pathname,
+      query: currentUrl,
+    },
+    { skipNull: true }
+  );
+}
+
+type RemoveQueryParams = {
+  params: string;
+  keysToRemove: string[];
+};
+
+export function removeKeysFromQueryParams({
+  params,
+  keysToRemove,
+}: RemoveQueryParams) {
+  const currentUrl = qs.parse(params);
+  keysToRemove.forEach((key) => {
+    delete currentUrl[key];
+  });
+  return qs.stringifyUrl(
+    {
+      url: window.location.pathname,
+      query: currentUrl,
+    },
+    {
+      skipNull: true,
+    }
+  );
 }
