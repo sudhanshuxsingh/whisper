b1e73dbd5062ec9ae447fadcbe5a3540fc360b46
s2-sengar <sudhanshu.iem2k18@gmail.com>
2024-09-26 08:37:36 +0530

    feat: make feedback card functional

--- src/app/(protected)/sphere/_features/FeedbackCard.tsx
+++ src/app/(protected)/sphere/_features/FeedbackCard.tsx
@@ -1,11 +1,26 @@
+import { Avatar } from '@/components/ui/avatar';
 import React from 'react';
+import USER_AVATAR from '@/assets/avatars/avatar.webp';
+import Image from 'next/image';
+type FeedbackCardProps = {
+  content: string;
+  userName?: string;
+  email?: string;
+};
 
-const FeedbackCard = () => {
+const FeedbackCard = ({ content, userName, email }: FeedbackCardProps) => {
   return (
-    <div className="h-60 w-full overflow-hidden text-ellipsis rounded-md border bg-background p-6 text-sm text-muted-foreground">
-      Every Supabase project comes with a full Postgres database, a free and
-      open source database which is considered one of the world&apos;s most
-      stable and advanced databases. Features# Table view# Y
+    <div className="h-48 w-full space-y-6 overflow-hidden text-ellipsis rounded-md border border-primary/10 bg-background p-6 text-sm text-muted-foreground hover:border-primary/20">
+      <div className="flex items-center gap-3">
+        <Avatar className="h-8 w-8">
+          <Image className="relative" src={USER_AVATAR} alt="avatar"></Image>
+        </Avatar>
+        <div className="">
+          <p className="text-primary">{userName ?? 'Annonymus'}</p>
+          <p className="text-xs">{email ?? 'annonymus@wshiper.com'}</p>
+        </div>
+      </div>
+      <p className="line-clamp-4 truncate text-wrap">{content}</p>
     </div>
   );
 };
