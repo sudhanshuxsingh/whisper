0afa0c4c05086b544ca827378bf8c613f964022c
s2-sengar <sudhanshu.iem2k18@gmail.com>
2024-09-29 18:36:14 +0530

    feat: add actions to modify/delete sphere, API Keys, fix: (check if sphere belongs to loggedin user)

--- src/lib/actions/sphere.actions.tsx
+++ src/lib/actions/sphere.actions.tsx
@@ -3,9 +3,11 @@ import dbConnect from '@/lib/db/dbConnect';
 import { SphereModel } from '@/model/sphere.model';
 import { auth } from '@clerk/nextjs/server';
 import { z } from 'zod';
-import { sphereSchema } from '@/schema/sphereSchema';
+import { sphereSchema, updateSphereSchema } from '@/schema/sphereSchema';
 import { nanoid } from 'nanoid';
-import { SphereProps } from '@/types/sphere.types';
+import { SphereProps, UpdateSphereProps } from '@/types/sphere.types';
+import { FeedbackModel } from '@/model/feedback.model';
+
 export async function createSphereAction(
   request: z.infer<typeof sphereSchema>
 ): Promise<SphereProps> {
@@ -58,9 +60,78 @@ export async function getSphereAction(sphereId: string): Promise<SphereProps> {
     if (sphere == null) {
       throw new Error(`No Sphere Found for ${sphereId}`);
     }
+    if (sphere.userId != userId) {
+      throw new Error(`Un-authorized`);
+    }
     return JSON.parse(JSON.stringify(sphere));
   } catch (error) {
     console.error('Error Fetching Sphere details for:', sphereId, error);
     throw new Error(`Failed to fetch sphere details for ${sphereId}`);
+  }
+}
+
+export async function modifySphereAction(
+  sphereId: string,
+  payload: UpdateSphereProps
+) {
+  try {
+    const parsedPayload = await updateSphereSchema.parseAsync(payload);
+    const { userId } = auth();
+    if (!userId) {
+      throw new Error('Unauthorized Request');
+    }
+    await dbConnect();
+    const sphere = await SphereModel.findById(sphereId);
+    if (sphere == null) {
+      throw new Error(`No Sphere Found for ${sphereId}`);
+    }
+    if (sphere.userId != userId) {
+      throw new Error(`Un-authorized`);
+    }
+    console.log({ sphereId, parsedPayload });
+    const updatedSphere = await SphereModel.findByIdAndUpdate(
+      sphereId,
+      parsedPayload
+    );
+    console.log({ updatedSphere });
+    return JSON.parse(JSON.stringify(updatedSphere));
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function generateNewApiKeyAction(sphereId: string) {
+  try {
+    const { userId } = auth();
+    if (!userId) {
+      throw new Error('Unauthorized Request');
+    }
+    await dbConnect();
+    const newApiKey = nanoid();
+    const res = await SphereModel.findByIdAndUpdate(sphereId, {
+      apiKey: newApiKey,
+    });
+    return JSON.parse(JSON.stringify(res));
+  } catch (error) {
+    console.error(error);
+    throw new Error('Failed to generate new API Key');
+  }
+}
+
+export async function deleteSphereAction(sphereId: string) {
+  try {
+    const { userId } = auth();
+    if (!userId) {
+      throw new Error('Unauthorized Request');
+    }
+    await dbConnect();
+    await FeedbackModel.deleteMany({ sphere: sphereId });
+    await SphereModel.findByIdAndDelete(sphereId);
+    return {
+      message: 'Sphere Deleted Successfully',
+    };
+  } catch (error) {
+    console.error(error);
+    throw new Error('Failed to delete sphere');
   }
 }
