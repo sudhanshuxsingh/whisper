5dc8684a26ec912da6a64a1713a4334b54d9cdb7
s2-sengar <sudhanshu.iem2k18@gmail.com>
2024-09-29 18:32:22 +0530

    fix: do validation if optional field present for feedback schema

--- src/schema/feedbackSchema.ts
+++ src/schema/feedbackSchema.ts
@@ -7,8 +7,20 @@ export const feedbackSchema = z.object({
     })
     .min(10, 'Message must be of at least 10 character')
     .max(300, 'Message can be of at max 300 characters'),
-  name: z.optional(z.string()),
-  email: z.optional(z.string()),
-  rating: z.optional(z.number()),
-  _id: z.optional(z.string()),
+  name: z
+    .string()
+    .min(3, {
+      message: 'Name must be of atleast 3 character',
+    })
+    .max(50, {
+      message: 'Name can be of at max 50 character',
+    })
+    .optional(),
+  email: z.string().email({ message: 'Invalid Email' }).optional(),
+  rating: z
+    .number()
+    .min(1, { message: "Rating value can't be less than 1" })
+    .max(5, { message: "Rating value can't be more than 5" })
+    .optional(),
+  _id: z.string().optional(),
 });
