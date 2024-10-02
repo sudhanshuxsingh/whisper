1e1e39a2651495324d2fe22094cdd12f4cf8c92f
s2-sengar <sudhanshu.iem2k18@gmail.com>
2024-10-02 18:18:40 +0530

    feat: add codeblock for sphere feedback submission api call

---
+++ src/app/(public)/_features/APICodeBlock.tsx
@@ -0,0 +1,33 @@
+import CodeWindow, { CodeWindowCode } from '@/components/ui/code-window';
+import React from 'react';
+const codeBlock = `var myHeaders = new Headers();
+myHeaders.append("X-API-KEY-WHISPER", "at43sds5usggsre4g");
+myHeaders.append("Content-Type", "application/json");
+
+var raw = JSON.stringify({
+  "content": "I loved it",
+  "name": "Sudhanshu Singh",
+  "email": "sudhanshu.iem2k18@gmail.com",
+  "rating": 5
+});
+
+var requestOptions = {
+  method: 'POST',
+  headers: myHeaders,
+  body: raw,
+  redirect: 'follow'
+};
+
+fetch("http://localhost:3000/api/v1/feedback", requestOptions)
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));`;
+const APICodeBlock = () => {
+  return (
+    <CodeWindow>
+      <CodeWindowCode code={codeBlock} language="tsx" overflow={false} />
+    </CodeWindow>
+  );
+};
+
+export default APICodeBlock;
