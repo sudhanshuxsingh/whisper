73224cd6b0fede45f68e1aeb476f648cfdcfe72d
s2-sengar <sudhanshu.iem2k18@gmail.com>
2024-10-08 22:50:26 +0530

    feat: add empty feedback container

--- src/app/(protected)/sphere/_features/EmptyFeedback.tsx
+++ src/app/(protected)/sphere/_features/EmptyFeedback.tsx
@@ -1,7 +1,105 @@
+'use client';
+import WhisperIcon from '@/components/logo/whisper';
+import { buttonVariants } from '@/components/ui/button';
+import { CopyToClipButtonV2 } from '@/components/ui/copy-to-clipboard-button';
+import { cn, getAbsoutePath } from '@/lib/utils';
+import {
+  CodeIcon,
+  CodeSandboxLogoIcon,
+  Component1Icon,
+} from '@radix-ui/react-icons';
+import { Link2Icon } from 'lucide-react';
+import Link from 'next/link';
+import { useParams, usePathname } from 'next/navigation';
 import React from 'react';
 
 const EmptyFeedback = () => {
-  return <div>EmptyFeedback</div>;
+  const params = useParams();
+  const pathname = usePathname();
+  const id = params.id as string | undefined;
+  return (
+    <div className="mt-14 grid place-items-center space-y-6 lg:mt-28">
+      <div className="grid aspect-square h-16 w-16 place-items-center rounded-md border bg-secondary/20">
+        <WhisperIcon className="h-full text-muted-foreground" />
+      </div>
+      <div className="space-y-2 text-center">
+        <p>Start collecting feedback/messages</p>
+        <p className="text-sm text-muted-foreground">
+          Choose from the options below to begin
+        </p>
+      </div>
+      <div className="rounded-sm border text-sm text-muted-foreground">
+        <div className="flex gap-6 border-b p-4">
+          <div className="grid aspect-square h-8 place-items-center rounded bg-secondary/40">
+            <Link2Icon className="h-4 w-4 -rotate-45" />
+          </div>
+          <div className="">
+            <p className="text-base text-primary">Link</p>
+            <p>Share the provided link</p>
+          </div>
+          <CopyToClipButtonV2
+            className="ml-auto"
+            text={getAbsoutePath(`whisper/${id}`)}
+            duration={2000}
+          />
+        </div>
+        <div className="flex gap-6 border-b p-4">
+          <div className="grid aspect-square h-8 place-items-center rounded bg-secondary/40">
+            <CodeSandboxLogoIcon className="h-4 w-4 -rotate-45" />
+          </div>
+          <div className="">
+            <p className="text-base text-primary">Web Embedd</p>
+            <p>Easily integrate widget into your web or app</p>
+          </div>
+          <Link
+            href={`${pathname}/embedd`}
+            className={cn(
+              buttonVariants({ variant: 'outline' }),
+              'my-auto ml-auto h-full rounded-sm border'
+            )}
+          >
+            View
+          </Link>
+        </div>
+        <div className="flex gap-6 border-b p-4">
+          <div className="grid aspect-square h-8 place-items-center rounded bg-secondary/40">
+            <CodeIcon className="h-4 w-4" />
+          </div>
+          <div className="">
+            <p className="text-base text-primary">API</p>
+            <p>Integrate using our flexible and easy-to-use API</p>
+          </div>
+          <Link
+            href={`${pathname}/api`}
+            className={cn(
+              buttonVariants({ variant: 'outline' }),
+              'my-auto ml-auto h-full rounded-sm border'
+            )}
+          >
+            View
+          </Link>
+        </div>
+        <div className="flex gap-6 p-4">
+          <div className="grid aspect-square h-8 place-items-center rounded bg-secondary/40">
+            <Component1Icon className="h-4 w-4" />
+          </div>
+          <div className="">
+            <p className="text-base text-primary">Primitive Component</p>
+            <p>Integrate using our flexible and easy-to-use API</p>
+          </div>
+          <Link
+            href={`${pathname}/component`}
+            className={cn(
+              buttonVariants({ variant: 'outline' }),
+              'my-auto ml-auto h-full rounded-sm border'
+            )}
+          >
+            View
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default EmptyFeedback;
--- src/components/ui/copy-to-clipboard-button.tsx
+++ src/components/ui/copy-to-clipboard-button.tsx
@@ -52,4 +52,45 @@ const SuccessButton = () => (
   </Button>
 );
 
+export const CopyToClipButtonV2 = ({
+  text,
+  duration = 2000,
+  className,
+}: CopyToClipboardButtonProps) => {
+  const { onCopy, hasCopied } = useClipboard(duration);
+  const handleClick = (event: React.MouseEvent<HTMLDivElement>) => {
+    if (!hasCopied) {
+      const rect = event.currentTarget.getBoundingClientRect();
+      const x = rect.left + rect.width / 2;
+      const y = rect.top + rect.height / 2;
+      confetti({
+        angle: 10,
+        gravity: 0.8,
+        scalar: 0.6,
+        origin: {
+          x: x / window.innerWidth,
+          y: y / window.innerHeight,
+        },
+      });
+    }
+    onCopy(text);
+  };
+  return (
+    <div onClick={handleClick} className={cn('h-fit w-fit', className)}>
+      {hasCopied ? (
+        <Button
+          className="h-9 min-w-[4.1rem] rounded-sm border"
+          variant={'outline'}
+        >
+          <CheckIcon className="h-4 w-4" />
+        </Button>
+      ) : (
+        <Button className="h-full rounded-sm border" variant={'outline'}>
+          Copy
+        </Button>
+      )}
+    </div>
+  );
+};
+
 export default CopyToClipButton;
