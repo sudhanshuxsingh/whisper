b285a58735e6c8e4c013be7724da1f2c375a2fd3
s2-sengar <sudhanshu.iem2k18@gmail.com>
2024-10-22 19:52:58 +0530

    refactor: remove unused loaders

--- src/app/(protected)/loading.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Loading = () => {
-  return <div>Loading... Private</div>;
-};
-
-export default Loading;
--- src/app/(public)/loading.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Loading = () => {
-  return <div>Loading Public</div>;
-};
-
-export default Loading;
