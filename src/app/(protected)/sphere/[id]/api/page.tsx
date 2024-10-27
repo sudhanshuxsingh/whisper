3ac91752332db8bc2dd127b40e3e5022ee666697
s2-sengar <sudhanshu.iem2k18@gmail.com>
2024-10-28 00:17:20 +0530

    feat: add api doc for feedback

--- src/app/(protected)/sphere/[id]/api/page.tsx
+++ src/app/(protected)/sphere/[id]/api/page.tsx
@@ -1,7 +1,29 @@
 import React from 'react';
-
+import { apidoc } from '#site/content';
+import { MDXContent } from '@/components/mdx/mdx-components';
+import Container from '@/components/ui/container';
+import APIKey from '../_features/APIKey';
 const Api = () => {
-  return <div>Api</div>;
+  const apiDoc = apidoc?.[0];
+  return (
+    <>
+      <div className="relative w-full border-b bg-secondary/10">
+        <Container className="max-w-5xl space-y-4 py-10">
+          <h1 className="text-xl md:text-2xl xl:text-3xl">REST API</h1>
+          <p className="text-muted-foreground">
+            The Whisper REST API is a REST-styled API that gives control over
+            the feedback collection process.
+          </p>
+        </Container>
+      </div>
+      <Container className="my-8 mb-24 grid max-w-5xl gap-8">
+        <APIKey />
+        <div className="">
+          {apiDoc?.content && <MDXContent code={apiDoc?.content} />}
+        </div>
+      </Container>
+    </>
+  );
 };
 
 export default Api;
