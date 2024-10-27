565af968366721ac9b96468f69aa324a7f03ba5e
s2-sengar <sudhanshu.iem2k18@gmail.com>
2024-10-27 23:29:47 +0530

    refactor: change sphere card ui styles

--- src/app/(protected)/overview/_features/sphere-card.tsx
+++ src/app/(protected)/overview/_features/sphere-card.tsx
@@ -25,9 +25,9 @@ const SphereCard = ({
   return (
     <Link
       href={`/sphere/${linkIdentifier}`}
-      className="group/dashboard-card rounded-xl bg-secondary p-1 pb-0"
+      className="group/dashboard-card rounded-xl bg-secondary/80 p-1 pb-0 dark:bg-secondary/40"
     >
-      <div className="relative flex h-5/6 flex-1 flex-col justify-between overflow-hidden rounded-lg border border-primary/15 bg-background/70 px-4 py-3 pt-6">
+      <div className="relative flex h-5/6 flex-1 flex-col justify-between overflow-hidden rounded-lg border border-primary/10 bg-background/70 px-4 py-3 pt-6">
         <div className="space-y-3">
           <div className="flex items-center gap-2 text-sm text-muted-foreground">
             <CrumpledPaperIcon />
@@ -46,8 +46,6 @@ const SphereCard = ({
             {description}
           </p>
         </div>
-
-        <div className="absolute -top-2 left-1/2 h-3 w-2/3 -translate-x-1/2 rounded-b-full bg-indigo-500"></div>
       </div>
       <div className="flex justify-between overflow-hidden px-3 py-2 text-xs font-medium text-muted-foreground">
         <span className="ease-&lsqb;cubic-bezier(0.2,0.4,0,1)&rsqb; transition group-hover/dashboard-card:-translate-x-[calc(100%+theme(spacing.4))] group-has-[a:focus-visible]:-translate-x-[calc(100%+theme(spacing.4))] motion-reduce:duration-0">
