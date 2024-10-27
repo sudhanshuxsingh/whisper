d930b59bc908767705c5b826eec44c8f15ec4756
s2-sengar <sudhanshu.iem2k18@gmail.com>
2024-10-27 23:48:59 +0530

    refactor: change sphere link style

--- src/app/(protected)/sphere/_features/SphereLink.tsx
+++ src/app/(protected)/sphere/_features/SphereLink.tsx
@@ -3,13 +3,9 @@ import CopyToClipButton from '@/components/ui/copy-to-clipboard-button';
 import React from 'react';
 const SphereLink = ({ link }: { link: string }) => {
   return (
-    <div className="relative flex w-full max-w-xl items-center gap-2 overflow-x-scroll rounded bg-secondary py-1 pl-4 pr-8 text-xs no-scrollbar md:w-fit">
+    <div className="relative flex w-full max-w-xl items-center gap-2 overflow-x-scroll rounded bg-secondary px-4 py-1 text-xs no-scrollbar md:w-fit">
       <pre className="text-muted-foreground">{link}</pre>
-      <CopyToClipButton
-        text={link}
-        duration={3000}
-        className="absolute right-2"
-      />
+      <CopyToClipButton text={link} duration={3000} className="" />
     </div>
   );
 };
